6041870d47fcbfe5a15daa72d9841c8d511a1142
Fabianhc91 <fhc-91@hotmail.com>
2026-02-21 22:06:28 -0500

    Agregar Empleado

---
+++ Tienda.API/Tienda.API/Empleado.cs
@@ -0,0 +1,9 @@
+﻿namespace Tienda.API
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public String  nombre { get; set; }
+        public int telefono { get; set; }
+    }
+}
